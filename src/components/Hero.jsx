f38b2a267d6087bb3a0389c13360abe1c8018692
Chris Dowd <chrisdowd1996@gmail.com>
2024-02-18 13:39:59 -0600

    application done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,22 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-        <button type='button' onClick={() => window.open('https://github.com/chrisdowddeveloper')}>
+
+        <button type='button' onClick={() => window.open('https://github.com/ChrisDowdDeveloper/AI-Summarizer')} className='black_btn'>
           GitHub
         </button>
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Web Pages with <br className='max-md:hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify with your reading with Summize, 
+        an open-source article summarizer that transforms lengthy articles into clear and consice summaries
+      </h2>
     </header>
   )
 }
